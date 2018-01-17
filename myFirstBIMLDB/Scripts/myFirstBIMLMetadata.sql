c43999361e7ec5257b04690e670c69dc48e16ce3
sarath <sarath@VM-WIN-DEV-SQL1>
2018-01-17 19:52:24 +1300

    Added BIML Etl segments, created environment, metadata, and stubs for staging

---
+++ myFirstBIMLDB/Scripts/myFirstBIMLMetadata.sql
@@ -0,0 +1,8 @@
+﻿/*
+ * We will just add a couple of rows here. In this case, we will be staging all AdventureWorks tables whose names begin with a 'Person'
+*/
+TRUNCATE TABLE myFirstBIMLDB.config.MyBIMLMeta_Tables;
+USE AdventureWorks2014;
+GO
+INSERT INTO myFirstBIMLDB.config.MyBIMLMeta_Tables
+SELECT DISTINCT name FROM sysobjects WHERE name LIKE 'Person%';
